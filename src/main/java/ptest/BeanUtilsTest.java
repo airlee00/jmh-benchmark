c52e192aab00b543b14dacf7702434d0b4d97bfc
SKMG <SKMG@SKMG-D>
2023-04-04 18:13:59 +0900

    asdfadf

--- src/main/java/ptest/BeanUtilsTest.java
+++ src/main/java/ptest/BeanUtilsTest.java
@@ -10,7 +10,8 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.BeanUtils;
 
 public class BeanUtilsTest {
- //테스트 입니다. 
+ //테스트 입니다.asdfadsf 
+	//adf
     public static void main(String[] args) throws IllegalAccessException, InvocationTargetException  {
 
     	Map<String, Object> map = createMapListForTest() ;
